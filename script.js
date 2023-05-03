08b4a0e723c1ed552856f38415e427b2c04f15d6
kelso1020 <kelseyjessica1710@gmail.com>
2023-05-03 15:35:56 -0400

    color coded timeblocks based on time of day

--- script.js
+++ script.js
@@ -9,15 +9,34 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
+  
+  // new variable to select the current hour of the current day with dayjs
+  var currentTime = dayjs().hour();
+
+  // function to change the color of each time block depending on what time it is
+  function changeColor() {
+    $('.time-block').each(function() {
+      const hour = parseInt(this.id);
+      if (hour === currentTime) {
+        $(this).addClass('present');
+      } else if (hour > currentTime) {
+        $(this).addClass('future');
+      } else {
+        $(this).addClass('past');
+      }
+    });
+  };
+
+  changeColor();
+
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  
+  // this selects and prints the current day of the week, month, date, and year on the webpage
+  var currentDay = dayjs();  
+  $('#currentDay').text(currentDay.format('dddd, MMMM D, YYYY'));
 });
